9d21b54d59584d8ae689eaa57c6d8218a3f33b97
Sami-Wafula <samiwafula3257@gmail.com>
2023-02-23 08:38:30 +0300

    edit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,11 +1,13 @@
 /**
- * dispays message param to output
+ * Displays a message on the STDOUT.
  * @param {String} message
  * @author Sami Wafula <https://github.com/Sami-Wafula>
  */
-
 const displayMessage = (msg) => {
 	console.log(msg);
-};
+  };
+  
+  module.exports = displayMessage;
 
-module.exports = displayMessage;
+ 
+
